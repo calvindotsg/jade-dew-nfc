35203146ea8eb54ce01f096c1f84e287b5f3ccd2
Calvin <68703834+calvindotsg@users.noreply.github.com>
2024-08-15 01:15:35 +0800

    rollback Modify the build configuration: Adjust the Rollup configuration to prevent externalization of the entry module.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
+import * as cheerio from 'cheerio';
 
 import solidJs from "@astrojs/solid-js";
 import {remarkReadingTime} from "./src/lib/remark-reading-time.mjs";
@@ -12,36 +13,11 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jadedew.netlify.app/',
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        'https://jadedew.netlify.app/sitemap-index.xml',
-        'https://jadedew.netlify.app/sitemap-0.xml',
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  output: 'server',
-  adapter: netlify({ edgeMiddleware: true }),
-  vite: {
-    build: {
-      rollupOptions: {
-        external: (id) => {
-          if (id.includes('@astrojs/netlify/ssr-function.js') || id.includes('@astro-renderers')) {
-            return false;
-          }
-          return id;
-        },
-      },
+    site: "https://jadedew.netlify.app/", integrations: [sitemap(), robotsTxt({
+        sitemap: ["https://jadedew.netlify.app/sitemap-index.xml", "https://jadedew.netlify.app/sitemap-0.xml",],
+    }), solidJs(), UnoCSS({injectReset: true}), icon(), svelte(),], markdown: {
+        remarkPlugins: [remarkReadingTime],
+    }, output: "server", adapter: netlify({edgeMiddleware: true}), vite: {
+        assetsInclude: "**/*.riv",
     },
-    assetsInclude: '**/*.riv',
-  },
 });
